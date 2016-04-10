29c5b9538b4b8732812c2a4cc2693559bb8659b3
semerdj <jsemer@gmail.com>
2016-04-10 11:46:39 -0700

    remove .decode("utf-8")

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -14,15 +14,15 @@ class TestList2XML(unittest.TestCase) :
     def test_none_tag(self):
         XMLequals( [('Box', 'foo'), ('#', 'Null'), ('1', 'foo'), ('Pinto', 'foo')], '<foo>Box</foo> <Null>#</Null> <foo>1</foo> <foo>Pinto</foo>')
         XMLequals( [('#', 'Null'), ('1', 'foo'), ('Pinto', 'foo')], '<Null>#</Null> <foo>1</foo> <foo>Pinto</foo>')
-       
-       
+
+
 def XMLequals(labeled_sequence, xml):
     correct_xml = '<TokenSequence>' + xml + '</TokenSequence>'
-    generated_xml = etree.tostring( sequence2XML(labeled_sequence, 'TokenSequence') ).decode('utf-8')
+    generated_xml = etree.tostring( sequence2XML(labeled_sequence, 'TokenSequence') )
     print('Correct:   %s' %correct_xml)
     print('Generated: %s' %generated_xml)
     assert correct_xml == generated_xml
 
 
 if __name__ == '__main__' :
-    unittest.main()    
+    unittest.main()
